ce236cdb7f78374b5d1d631b1e51210a2b9b5e40
afeiluo <352172935@qq.com>
2016-10-14 10:23:47 +0800

    增加计算地理位置距离的类

---
+++ src/main/java/com/afeiluo/util/EarthDistanceUtil.java
@@ -0,0 +1,31 @@
+package com.afeiluo.util;
+
+/**
+ * Created by ben on 16/10/14.
+ */
+public class EarthDistanceUtil {
+    private static final double EARTH_RADIUS = 6378.137;
+
+    private static double rad(double d) {
+        return d * Math.PI / 180.0;
+    }
+
+    public static double getDistance(double long1, double lat1, double long2, double lat2) {
+        double a, b, d, sa2, sb2;
+        lat1 = rad(lat1);
+        lat2 = rad(lat2);
+        a = lat1 - lat2;
+        b = rad(long1 - long2);
+
+        sa2 = Math.sin(a / 2.0);
+        sb2 = Math.sin(b / 2.0);
+        d = 2 * EARTH_RADIUS
+                * Math.asin(Math.sqrt(sa2 * sa2 + Math.cos(lat1)
+                * Math.cos(lat2) * sb2 * sb2));
+        return d;
+    }
+
+    public static void main(String[] args) {
+        System.out.println("beijing and shanghai distance:" + getDistance(116.41667, 39.91667, 113.23333,23.16667));
+    }
+}
