5d7111a9fd3514ccb75d501649907219c2c53167
afeiluo <352172935@qq.com>
2017-01-09 10:45:13 +0800

    增加thrift的异步客户端的demo

---
+++ src/main/java/com/afeiluo/thrift/origin/client/AsyncClientDemo.java
@@ -0,0 +1,86 @@
+package com.afeiluo.thrift.origin.client;
+
+import java.util.concurrent.CountDownLatch;
+import java.util.concurrent.TimeUnit;
+
+import org.apache.thrift.async.AsyncMethodCallback;
+import org.apache.thrift.async.TAsyncClientManager;
+import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.protocol.TProtocolFactory;
+import org.apache.thrift.transport.TNonblockingSocket;
+import org.apache.thrift.transport.TNonblockingTransport;
+
+import com.afeiluo.thrift.origin.inter.TestService;
+import com.afeiluo.thrift.origin.inter.TestService.AsyncClient;
+import com.afeiluo.thrift.origin.inter.TestService.AsyncClient.findPerson_call;
+
+/**
+ * 异步客户端
+ * 
+ * @author qiaolinfei
+ * 
+ */
+public class AsyncClientDemo {
+    public static final String SERVER_IP = "localhost";
+    public static final int SERVER_PORT = 8090;
+    public static final int TIMEOUT = 30000;
+
+    /**
+     * 
+     * @param userName
+     */
+    public void startClient(String userName) {
+        try {
+            TAsyncClientManager clientManager = new TAsyncClientManager();
+            TNonblockingTransport transport = new TNonblockingSocket(SERVER_IP, SERVER_PORT, TIMEOUT);
+            // 协议要和服务端一致
+            TProtocolFactory tprotocol = new TCompactProtocol.Factory();
+            TestService.AsyncClient asyncClient = new TestService.AsyncClient(tprotocol, clientManager, transport);
+            System.out.println("Client start .....");
+
+            CountDownLatch latch = new CountDownLatch(1);
+            AsynCallback callBack = new AsynCallback(latch);
+            System.out.println("call method findPerson start ...");
+            asyncClient.findPerson(111L, callBack);
+            System.out.println("call method findPerson .... end");
+            boolean wait = latch.await(30, TimeUnit.SECONDS);
+            System.out.println("latch.await =:" + wait);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        System.out.println("startClient end.");
+    }
+
+    public class AsynCallback implements AsyncMethodCallback<AsyncClient.findPerson_call> {
+        private CountDownLatch latch;
+
+        public AsynCallback(CountDownLatch latch) {
+            this.latch = latch;
+        }
+
+        @Override
+        public void onError(Exception exception) {
+            System.out.println("onError :" + exception.getMessage());
+            latch.countDown();
+        }
+
+        @Override
+        public void onComplete(findPerson_call response) {
+            try {
+                System.out.println(response.getResult().toString());
+            } catch (Exception e) {
+                e.printStackTrace();
+            } finally {
+                latch.countDown();
+            }
+        }
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        AsyncClientDemo client = new AsyncClientDemo();
+        client.startClient("Michael");
+    }
+}
---
+++ src/main/java/com/afeiluo/thrift/origin/server/ServerDemo.java
@@ -0,0 +1,70 @@
+package com.afeiluo.thrift.origin.server;
+
+import org.apache.thrift.TProcessor;
+import org.apache.thrift.protocol.TCompactProtocol;
+import org.apache.thrift.server.TNonblockingServer;
+import org.apache.thrift.server.TServer;
+import org.apache.thrift.transport.TFramedTransport;
+import org.apache.thrift.transport.TNonblockingServerSocket;
+
+import com.afeiluo.thrift.origin.inter.TestService;
+import com.afeiluo.thrift.origin.inter.impl.TestServiceImpl;
+
+public class ServerDemo {
+
+    public static final int SERVER_PORT = 8090;
+
+    public void startServer() {
+        try {
+            System.out.println("ServerDemo start ....");
+            TProcessor tprocessor = new TestService.Processor<TestServiceImpl>(new TestServiceImpl());
+            
+            
+            // 简单的单线程服务模型，一般用于测试
+            // TServerSocket serverTransport = new TServerSocket(SERVER_PORT);
+            // TServer.Args tArgs = new TServer.Args(serverTransport);
+            // tArgs.processor(tprocessor);
+            // tArgs.protocolFactory(new TBinaryProtocol.Factory());
+            // // tArgs.protocolFactory(new TCompactProtocol.Factory());
+            // // tArgs.protocolFactory(new TJSONProtocol.Factory());
+            // TServer server = new TSimpleServer(tArgs);
+
+            // 线程池服务模型，使用标准的阻塞式IO，预先创建一组线程处理请求。
+            // TServer server = new TThreadPoolServer(new
+            // TThreadPoolServer.Args(serverTransport).processor(tprocessor));
+
+            // 使用非阻塞式IO，服务端和客户端需要指定 TFramedTransport 数据传输的方式。
+            TNonblockingServerSocket tnbSocketTransport = new TNonblockingServerSocket(SERVER_PORT);
+            TNonblockingServer.Args tnbArgs = new TNonblockingServer.Args(tnbSocketTransport);
+            tnbArgs.processor(tprocessor);
+            tnbArgs.transportFactory(new TFramedTransport.Factory());
+            tnbArgs.protocolFactory(new TCompactProtocol.Factory());
+            TServer server = new TNonblockingServer(tnbArgs);
+
+            
+            //半同步半异步的服务端模型，需要指定为： TFramedTransport 数据传输的方式。
+            // TNonblockingServerSocket tnbSocketTransport = new TNonblockingServerSocket(
+            // SERVER_PORT);
+            // THsHaServer.Args thhsArgs = new THsHaServer.Args(tnbSocketTransport);
+            // thhsArgs.processor(tprocessor);
+            // thhsArgs.transportFactory(new TFramedTransport.Factory());
+            // thhsArgs.protocolFactory(new TBinaryProtocol.Factory());
+            // TServer server = new THsHaServer(thhsArgs);
+            
+            
+            server.serve();
+        } catch (Exception e) {
+            System.out.println("Server start error!!!");
+            e.printStackTrace();
+        }
+    }
+
+    /**
+     * @param args
+     */
+    public static void main(String[] args) {
+        ServerDemo server = new ServerDemo();
+        server.startServer();
+    }
+
+}
