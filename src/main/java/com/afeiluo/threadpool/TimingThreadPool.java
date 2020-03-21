a9eae7d49fde21128054127617e7b40ae32ec9c3
qiaolinfei <qiaolinfei@qiaolinfeideMacBook-Pro.local>
2020-03-21 23:54:22 +0800

    customize threadpoolexecutor

---
+++ src/main/java/com/afeiluo/threadpool/TimingThreadPool.java
@@ -0,0 +1,55 @@
+package com.afeiluo.threadpool;
+
+import org.omg.PortableInterceptor.SYSTEM_EXCEPTION;
+
+import java.util.concurrent.BlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+import java.util.concurrent.atomic.AtomicLong;
+import java.util.logging.Logger;
+
+/**
+ * 通过扩展ThreadPoolExecutor的生命周期方法来定制线程池
+ * Created by qiaolinfei on 2020/3/21.
+ */
+public class TimingThreadPool extends ThreadPoolExecutor {
+    private final ThreadLocal<Long> startTime = new ThreadLocal<Long>();
+    private final Logger log = Logger.getLogger("TimingThreadPool");
+    private final AtomicLong numTasks = new AtomicLong();
+    private final AtomicLong totalTime = new AtomicLong();
+
+
+    public TimingThreadPool(int corePoolSize, int maximumPoolSize, long keepAliveTime, TimeUnit unit, BlockingQueue<Runnable> workQueue) {
+        super(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue);
+    }
+    // TODO 根据需要重写ThreadPoolExecutor所有的构造方法
+
+    @Override
+    protected void beforeExecute(Thread t, Runnable r) {
+        super.beforeExecute(t, r);
+        log.fine(String.format("Thread %s: start %s", t, r));
+        startTime.set(System.nanoTime());
+    }
+
+    @Override
+    protected void afterExecute(Runnable r, Throwable t) {
+        try {
+            long endTime = System.nanoTime();
+            long taskTime = endTime - startTime.get();
+            numTasks.incrementAndGet();
+            totalTime.addAndGet(taskTime);
+            log.fine(String.format("Thread %s: end %s, time=%dbs", t, r, taskTime));
+        } finally {
+            super.afterExecute(r, t);
+        }
+    }
+
+    @Override
+    protected void terminated() {
+        try {
+            log.info(String.format("Terminated: avg time=%dns", totalTime.get() / numTasks.get()));
+        } finally {
+            super.terminated();
+        }
+    }
+}
